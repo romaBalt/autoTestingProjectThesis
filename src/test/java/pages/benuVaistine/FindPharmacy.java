ff738b965c244b1c52c1bbfc71ca3f33422d3de8
romaBalt <108835882+romaBalt@users.noreply.github.com>
2022-07-22 21:11:07 +0300

    added test for FindPharmacy

--- src/test/java/pages/benuVaistine/FindPharmacy.java
+++ src/test/java/pages/benuVaistine/FindPharmacy.java
@@ -1,4 +1,48 @@
 package pages.benuVaistine;
 
+import pages.Common;
+import pages.Locators;
+
 public class FindPharmacy {
+
+    public static void open() {
+        pages.Common.openUrl("https://www.benu.lt/");
+    }
+
+    public static void closeCookies() {
+        Common.waitForCookiesFormToBeVisible(Locators.BenuVaistine.HomePage.formCookies);
+        Common.clickElement(Locators.BenuVaistine.HomePage.buttonCloseCookies);
+    }
+
+    public static void closeAdd() {
+        Common.waitForAddFormToBeVisible(Locators.BenuVaistine.HomePage.formAdd);
+        Common.clickElement(Locators.BenuVaistine.HomePage.buttonCloseAdd);
+
+    }
+
+    public static void enterPharmacySearch() {
+        pages.Common.waitForElementToBeCickable(Locators.BenuVaistine.FindPharmacy.buttonPharmacySearch);
+        pages.Common.clickByAction(Locators.BenuVaistine.FindPharmacy.buttonPharmacySearch);
+
+    }
+
+    public static void enterCitySearch() {
+        pages.Common.clickByAction(Locators.BenuVaistine.FindPharmacy.buttonSearchCity);
+
+    }
+
+    public static void searchCityFromList() {
+        pages.Common.clickByAction(Locators.BenuVaistine.FindPharmacy.chooseCityFromList);
+
+    }
+
+    public static void checkBox() {
+        pages.Common.clickByAction(Locators.BenuVaistine.FindPharmacy.checkBoxOnDuty);
+
+    }
+
+    public static void clickOnResult() {
+        pages.Common.clickByAction(Locators.BenuVaistine.FindPharmacy.chooseResult);
+
+    }
 }
--- src/test/java/tests/benuVaistine/FindPharmacy.java
+++ src/test/java/tests/benuVaistine/FindPharmacy.java
@@ -1,4 +1,29 @@
 package tests.benuVaistine;
 
-public class FindPharmacy {
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import tests.BaseTest;
+
+public class FindPharmacy extends BaseTest {
+
+    @BeforeMethod
+    @Override
+    public void setUp() {
+        super.setUp();
+        pages.benuVaistine.FindPharmacy.open();
+        pages.benuVaistine.FindPharmacy.closeCookies();
+        pages.benuVaistine.FindPharmacy.closeAdd();
+    }
+
+
+    @Test
+    public void testingPharmacySearch(){
+
+        pages.benuVaistine.FindPharmacy.enterPharmacySearch();
+        pages.benuVaistine.FindPharmacy.enterCitySearch();
+        pages.benuVaistine.FindPharmacy.searchCityFromList();
+        pages.benuVaistine.FindPharmacy.checkBox();
+        pages.benuVaistine.FindPharmacy.clickOnResult();
+
+    }
 }
