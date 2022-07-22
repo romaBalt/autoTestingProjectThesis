49a765506e69eb2ebadf1e51462cf9d4758d7f01
romaBalt <108835882+romaBalt@users.noreply.github.com>
2022-07-22 10:35:42 +0300

    added baseTest and Constants

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import utils.Driver;
+import utils.TestListener;
+
+@Listeners(TestListener.class)
+public class BaseTest {
+
+    @BeforeMethod
+    public void setUp(){Driver.setDriver();}
+
+//    @AfterMethod
+//    public void teardown() {Driver.closeDriver();}
+
+
+
+
+}
---
+++ src/test/java/utils/Constants.java
@@ -0,0 +1,7 @@
+package utils;
+
+import java.time.Duration;
+
+public class Constants {
+    public static final Duration DURATION_TIMEOUT = Duration.ofSeconds(10);
+}
