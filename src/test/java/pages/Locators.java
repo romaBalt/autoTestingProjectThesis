1c9c246da44a3f74b09c0db6d7e50b819a582690
romaBalt <108835882+romaBalt@users.noreply.github.com>
2022-07-22 10:36:13 +0300

    added Locators

---
+++ src/test/java/pages/Locators.java
@@ -0,0 +1,33 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+public class Locators {
+
+    public static class BenuVaistine {
+        public static class HomePage {
+
+            public static By formCookies = By.xpath("//*[@id=\"CybotCookiebotDialogBody\"]");
+            public static By buttonCloseCookies = By.xpath("//*[@id=\"CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll\"]");
+            public static By formAdd = By.xpath("//*[@id=\"popupBanner-2584\"]/div/div/div[1]/div/div/a/img");
+            public static By buttonCloseAdd = By.xpath("//*[@id=\"popupBanner-2584\"]/div/div/button/i");
+            public static By fieldSearchClicK = By.xpath("//*[@id=\"headerSearch\"]");
+            public static By inputSearchItem = By.xpath("//*[@id=\"headerSearch\"]");
+//            public static By selectItemFromTheList = By.xpath("//*[@id=\"tt-4917af14-69a3-d510-fc05-33b96000afeb\"]");
+//            public static By clickOnItem = By.xpath("//*[@id=\"tt-a420b030-7499-abde-3103-97c306536087\"]");
+
+//            public static By doubleClickOnItem = By.xpath("");
+//
+//            public static By clickOnItem = By.xpath("");
+//            public static By searchItem = By.xpath("");
+//
+//            public static By buttonSearch = By.xpath("");
+//
+////
+//        }
+//
+
+
+       }
+    }
+}
