104d9d06dbc7a2560a4e16f96d9acaaf48afd2ce
romaBalt <108835882+romaBalt@users.noreply.github.com>
2022-07-22 19:19:27 +0300

    created class ShoppingCart to pages and tests

---
+++ src/test/java/pages/benuVaistine/ShoppingCart.java
@@ -0,0 +1,44 @@
+package pages.benuVaistine;
+
+import pages.Common;
+import pages.Locators;
+
+public class ShoppingCart {
+
+    public static void open() {
+        pages.Common.openUrl("https://www.benu.lt/");
+    }
+
+    public static void closeCookies() {
+        Common.waitForCookiesFormToBeVisible(Locators.BenuVaistine.HomePage.formCookies);
+        Common.clickElement(Locators.BenuVaistine.HomePage.buttonCloseCookies);
+    }
+
+    public static void closeAdd() {
+        Common.waitForAddFormToBeVisible(Locators.BenuVaistine.HomePage.formAdd);
+        Common.clickElement(Locators.BenuVaistine.HomePage.buttonCloseAdd);
+
+    }
+
+    public static void enterToSale() {
+        pages.Common.clickByAction(Locators.BenuVaistine.ShoppingCart.goToSaleItems);
+
+    }
+
+    public static void chooseItemFromSale() {
+        pages.Common.clickByAction(Locators.BenuVaistine.ShoppingCart.selectItemFromSale);
+
+    }
+    public static void clickItemToCartButton() {
+        pages.Common.clickByAction(Locators.BenuVaistine.ShoppingCart.buttonPutItemToCart);
+    }
+
+    public static void clickViewCartButton() {
+        pages.Common.clickByAction(Locators.BenuVaistine.ShoppingCart.buttonViewCart);
+    }
+
+    public static String readCartMessage(){
+        return Common.getElementText(Locators.BenuVaistine.ShoppingCart.fieldCartMessage);
+
+    }
+}
---
+++ src/test/java/tests/benuVaistine/ShoppingCart.java
@@ -0,0 +1,35 @@
+package tests.benuVaistine;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import tests.BaseTest;
+
+public class ShoppingCart extends BaseTest {
+
+    @BeforeMethod
+    @Override
+    public void setUp() {
+        super.setUp();
+        pages.benuVaistine.ShoppingCart.open();
+        pages.benuVaistine.ShoppingCart.closeCookies();
+        pages.benuVaistine.ShoppingCart.closeAdd();
+    }
+
+    @Test
+    public void testingAddToCart() {
+        String expectedCartMessage = "Jūsų krepšelyje yra 1 prekės";
+        String actualCartMessage;
+
+        pages.benuVaistine.ShoppingCart.enterToSale();
+        pages.benuVaistine.ShoppingCart.chooseItemFromSale();
+        pages.benuVaistine.ShoppingCart.clickItemToCartButton();
+        pages.benuVaistine.ShoppingCart.clickViewCartButton();
+        actualCartMessage = pages.benuVaistine.ShoppingCart.readCartMessage();
+
+
+        Assert.assertEquals(actualCartMessage, expectedCartMessage);
+
+    }
+
+}
